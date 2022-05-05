16aada105437d1d63ea0dfcad76c24674a2ded51
Bhargav Annem <bhargav.annem@gmail.com>
2022-05-05 15:55:58 -0500

    testing ray tracing

--- default.frag
+++ default.frag
@@ -9,9 +9,73 @@ uniform float width;
 uniform vec3 cameraPosition;
 uniform vec2 u_resolution;
 
-vec3 boxMax = {50.0f, 50.0f, 50.0f};
-vec4 background = {0.82745098039f, 0.82745098039f, 0.82745098039f, 1.0f};
+vec4 BACKGROUND = {0.82745098039f, 0.82745098039f, 0.82745098039f, 1.0f};
+vec4 BLACK = {0.0f, 0.0f, 0.0f, 1.0f};
+vec4 WHITE = {1.0f, 1.0f, 1.0f, 1.0f};
 vec2 fc = gl_FragCoord.xy/u_resolution;
+float gwidth = 0.5f;
+float step = 0.01f;
+
+struct Ray{
+	float x;
+	float y;
+	float z;
+	bool isHit;
+};
+
+vec3 toVec3(Ray r){
+	return vec3(r.x, r.y, r.z);
+}
+
+float mag(Ray r){
+	return sqrt(
+				pow(r.x, 2)
+			  + pow(r.y, 2)
+			  + pow(r.z, 2)
+			  );
+}
+float mag(vec3 r){
+	return sqrt(
+				pow(r.x, 2)
+			  + pow(r.y, 2)
+			  + pow(r.z, 2)
+			  );
+}
+
+float mag(vec2 r){
+	return sqrt(
+				pow(r.x, 2)
+				+ pow(r.y, 2)
+				);
+}
+
+vec3 normalize(vec3 v){
+	return v / mag(v);
+}
+
+vec2 normalize(vec2 v){
+	return v / mag(v);
+}
+
+
+
+bool inCube(vec3 v, float cube){
+	return  -cube <= v.x && v.x <= cube && 
+			-cube <= v.y && v.y <= cube && 
+			-cube <= v.z && v.z <= cube;
+}
+
+Ray divide(Ray r, float d){
+	Ray ans = Ray(r.x / d, r.y / d, r.z / d, r.isHit);
+	return ans;
+}
+
+Ray invert(Ray r){
+	r.x *= -1;
+	r.y *= -1;
+	r.z *= -1;
+	return r;
+}
 
 bool isFilled(vec4 v){
 	float th = 0.005;
@@ -23,20 +87,63 @@ bool check(int i, int j, int k){
 	return isFilled(texelFetch(pixels, ivec2(i, floor(width) * j + k), 0));
 }
 
-vec4 RayMarching(vec4 coord){
-	return 0.95f * coord;
+int clamp(float val, int limit){
+	
+	float threshold = 0.01f;
+	if(val < threshold) return 0;
+	else if(val > float(limit) - threshold) return limit - 1;
+	int ans = int(ceil(val)) * limit;
+	if(val > 0.5) return 0;
+	return min(limit, ans);
+}
+
+bool getPosition(vec3 v){
+	int i = clamp(v.x, int(width));
+	int j = clamp(v.y, int(width));
+	int k = clamp(v.z, int(width));
+
+	if(i == 0) return true;
+
+	return check(i,j,k);
+	
+}
+
+
+vec4 RayMarching(vec2 crd, float st){
+	vec2 coord = normalize(crd);
+	vec3 cp = normalize(vec3(cameraPosition.x, cameraPosition.y, cameraPosition.z));
+	Ray r = Ray(cp.x - coord.x,
+		  cp.y - coord.y,
+		  cp.z,
+		  false);
+	r = invert(r);
+	float current_step = 0.0f;
+	bool hit = false;
+	// CameraPosition: (0,0,1)
+	vec3 projectedRay = cameraPosition + toVec3(r) * current_step; 
+	while(inCube(projectedRay, width) && current_step < 100.0f){
+		if(getPosition(projectedRay)){
+			r.isHit = true;
+		}
+		current_step += 0.01f;
+		projectedRay = cameraPosition + toVec3(r) * st; 
+	}
+	if(r.isHit){
+		return vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+	return vec4(r.x, r.y, r.z, 1.0f);
 }
 
 
 void main()
 {
-	if(-0.5f < pos.x && pos.x < 0.5f && -0.5f < pos.y && pos.y < 0.5f){
+	if(-gwidth < pos.x && pos.x < gwidth && -gwidth < pos.y && pos.y < gwidth){
 		//FragColor = vec4(texture(pixels, texPos));
-		
-		FragColor = RayMarching(gl_FragCoord);
+		vec2 coord = vec2(gl_FragCoord.x, gl_FragCoord.y) / u_resolution;
+		FragColor = RayMarching(coord, 0.001f);
 	}
 	else{
-		FragColor = background;
+		FragColor = BACKGROUND;
 	}
 
 		
--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ GLuint indices[] =
 	2, 3, 0
 };
 
-
+const float dimension = 2.0f;
 
 int main()
 {
@@ -54,39 +54,14 @@ int main()
 	Shader shaderProgram("default.vert", "default.frag");
 
 	float data[] = {
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
+
 	};
 
-	Texture t(data, 5, 25);
+	Texture t(data, int(dimension), int(dimension) * int(dimension));
 	t.BindTexture();
 	t.applyTexture();
 
@@ -97,14 +72,14 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 5 * sizeof(float), (void*)(0));
-	VAO1.LinkAttrib(VBO1, 1, 2, GL_FLOAT, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 1, 2, GL_FLOAT, int(dimension) * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
 
 	// Creates camera object
-	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 1.0f));
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -115,7 +90,7 @@ int main()
 		camera.Inputs(window);
 		// Updates and exports the camera matrix to the Vertex Shader
 		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
-		glUniform1f(glGetUniformLocation(shaderProgram.ID, "width"), 1.0f);
+		glUniform1f(glGetUniformLocation(shaderProgram.ID, "width"), dimension);
 		glUniform3f(glGetUniformLocation(shaderProgram.ID, "cameraPosition"), camera.Position.x, camera.Position.y, camera.Position.z);
 		glUniform2f(glGetUniformLocation(shaderProgram.ID, "u_resolution"), width, height);
 		VAO1.Bind();
